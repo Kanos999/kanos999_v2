8e411de04a6b670efed4e15590430a2df7d9386f
Kane Jackson <kane@kanejackson.com>
2025-01-24 21:40:58 +1100

    Not found and optimised for desktop

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -33,28 +33,22 @@ const career = [
   {
     position: "Mechanical Engineering Intern",
     company: "ANT61",
-    description: [
-      "Designed mechanical fixtures for validating our product in a vibration test, qualifying it for space flight.",
-      "Facilitated thermal cycling tests (vacuum and in-air) in accordance with SpaceX qualification standards.",
-      "Analytically verified designs of product enclosures and its overall mechanical integrity."
-    ]
+    description: "• Designed mechanical fixtures for validating our product in a vibration test, qualifying it for space flight. \n\
+                  • Facilitated thermal cycling tests (both in vacuum and in-air) on the same product in accordance with SpaceX qualification standards. \n\
+                  • Analytically verified designs of product enclosures and its overall mechanical integrity.\n"
   },
   {
     position: "Lead Software Engineer",
     company: "InnerSteps",
-    description: [
-      "Coordinated with a team of skilled engineers to deliver a high-quality, child-friendly mobile app.",
-      "Produced an MVP in a few months, leading to a user base growth of over 700.",
-      "Implemented agile methodologies to ensure efficient project delivery and meet tight deadlines."
-    ]
+    description: "• Coordinated with a team of skilled engineers to deliver a high-quality, child-friendly mobile app for InnerSteps. \n\
+                  • Produced an MVP in a few months, leading to a user base growth of over 700. \n\
+                  • Implemented agile methodologies to ensure efficient project delivery and meet tight deadlines.\n"
   },
   {
     position: "Software Engineer",
     company: "Gaming Entertainment Systems",
-    description: [
-      "Implemented engaging visual displays using HTML, CSS and JavaScript.",
-      "Designed printable CAD models to enhance product presentations for potential clients."
-    ]
+    description: "• Implemented engaging visual displays using HTML, CSS and JavaScript. \n\
+                  • Designed printable CAD models to enhance product presentations for potential clients.\n"
   }
 ];
 
@@ -69,71 +63,11 @@ export default function Home() {
         <div className={courier.className}>
           <Background perlin={perlin} currentSection={currentSection} setCurrentSection={setCurrentSection} />
         </div>
+      </div>
 
-        <div className={`${poppins.className} absolute top-0 flex flex-col justify-center max-w-4xl left-0 right-0 mx-auto`}>
-          <div className=" w-full md:top-12">
-
-            {/* border-sky-500/10 bg-sky-900/20 */}
-            <div className="rounded-md px-12 mt-48 mb-6  w-full text-white"> 
-              {/* <div className="rounded-full bg-gray-700 aspect-square h-6 my-4 left-0 right-0 mx-auto"></div>
-              <div className="w-full h-12 bg-gray-300"></div> */}
-              <div className={`${cedarville.className} text-md  italic text-white/80`}>Hi! My name is</div>
-              <div className={poppinsBold.className}><div className="text-4xl">Kane Jackson</div></div>
-              <div className="text-md mt-4">Mechatronics / Computer Science</div>
-            </div>
-
-            <div className="bg-white w-full p-12 rounded-tr-3xl rounded-bl-3xl md:rounded-3xl text-zinc-950/80 relative z-30">
-              {/* Links */}
-              <div className="flex flex-row mb-10">
-                <a href="https://www.linkedin.com/in/kanehjackson/">
-                  <img src="linkedin.png" className="opacity-50 mr-6 h-8" alt="Kane Jackson on LinkedIn" />
-                </a>
-                <a href="https://github.com/Kanos999">
-                  <img src="github.png" className="opacity-50 mr-4 h-10 -mt-1" alt="Kane Jackson on GitHub" />
-                </a>
-                <a href="https://www.instagram.com/kane.json/">
-                  <img src="instagram.png" className="opacity-30 mr-6 h-12 -mt-2" alt="Kane Jackson on Instagram" />
-                </a>
-              </div>
-
-              <div className={`${poppinsBold.className} text-2xl mb-8 font-bold`}>About me</div>
-              <div className="text-justify">
-                Passionate about leveraging software development and robotics to drive innovation in the space industry. 
-                My journey as a coder and problem-solver has been fueled by a fascination with the limitless possibilities 
-                of technology and a deep admiration for the extraordinary accomplishments of the aerospace sector &#128640;
-              </div>
-              
-              <div className="w-full h-1 border-t border-t-zinc-950/20 my-8 md:my-12"></div>
-
-              <div className={`${poppinsBold.className} text-2xl mb-6 md:mb-8 font-bold`}>Career</div>
-              {career.map((job, i) => {
-                return (
-                  <div key={i} className="grid grid-cols-1 md:grid-cols-2 gap-4 divide-y divide-gray-200 md:divide-y-0">
-                    {/* Job title, position, company, dates etc */}
-                    <div className="" onClick={() => { setOpenCareerDescription(openCareerDescription == i ? -1 : i) }}>
-                      <div className="font-bold">{job.position}</div>
-                      <div className="italic">{job.company}</div>
-                    </div>
-
-                    {/* Description */}
-                    <div className={`${openCareerDescription == i ? "max-h-96 opacity-100" : "max-h-0 opacity-0"} transition-all duration-500 ease-in-out overflow-y-hidden pl-4 md:max-h-96 md:opacity-100 md:mb-12`}>
-                      <ul className="py-4 md:py-0 text-zinc-500 list-disc">
-                        {job.description.map((descriptionItem, j) => {
-                          return <li className="list-disc " key={j}>{descriptionItem}</li>;
-                        })}
-                      </ul>
-                      {/* <div className="w-full h-1 border-t border-t-zinc-950/20 my-2"></div> */}
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-
-            <div className="text-white p-12 text-center">{"Made with <3 by @Kanos999"}</div>
-
-          </div>
-          
-        </div>
+      <div className="z-40 text-white font-bold text-center absolute h-full w-full flex flex-col justify-center">
+        <div className="text-2xl">Umm... this is awkward</div>
+        <div className="text-xl text-zinc-500">404 Not Found</div>
       </div>
     </main>
   );
